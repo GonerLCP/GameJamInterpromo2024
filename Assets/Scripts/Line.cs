69585e71b6c76f16fda799b71461bf70a9b7b9a9
GonerLCP <antonin.arnold54700@gmail.com>
2024-12-16 22:13:56 -0500

    drawline

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using UnityEditor.TerrainTools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Line : MonoBehaviour
+{
+    public LineRenderer lineRend;
+    List<Vector2> points;
+    public void Updateline(Vector2 position)
+    {
+        if (points == null)
+        {
+            points = new List<Vector2>();
+            SetPoint(position);
+            return;
+        } 
+        if(Vector2.Distance(points.Last(), position)> .1f)
+        {
+            SetPoint(position);
+        }
+    }
+    void SetPoint(Vector2 point)
+    {
+        points.Add(point);
+
+        lineRend.positionCount = points.Count;
+        lineRend.SetPosition(points.Count - 1, point);
+    }
+}
